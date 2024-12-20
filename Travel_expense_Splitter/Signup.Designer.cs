dde139a1427f054e7538bdfb38130e9bc544b850
ChinmayGawad <chinmaygawad86@gmail.com>
2024-12-20 11:23:40 +0530

    Sign Up Done

--- Travel_expense_Splitter/Signup.Designer.cs
+++ Travel_expense_Splitter/Signup.Designer.cs
@@ -33,12 +33,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -51,9 +51,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(227, 19);
+            this.label1.Location = new System.Drawing.Point(303, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(359, 31);
+            this.label1.Size = new System.Drawing.Size(450, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "User Registration or Sign Up";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -61,9 +62,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(77, 43);
+            this.label2.Location = new System.Drawing.Point(103, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.Size = new System.Drawing.Size(107, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter your Name";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -71,18 +73,20 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(80, 95);
+            this.label3.Location = new System.Drawing.Point(107, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.Size = new System.Drawing.Size(104, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Enter your Email";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(80, 143);
+            this.label4.Location = new System.Drawing.Point(107, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.Size = new System.Drawing.Size(128, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "Enter new Password";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -90,89 +94,100 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 201);
+            this.label5.Location = new System.Drawing.Point(103, 247);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.Size = new System.Drawing.Size(144, 16);
             this.label5.TabIndex = 4;
             this.label5.Text = "Confirm your Password";
             // 
-            // textBox1
+            // tbName
             // 
-            this.textBox1.Location = new System.Drawing.Point(337, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 20);
-            this.textBox1.TabIndex = 5;
+            this.tbName.Location = new System.Drawing.Point(449, 49);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(292, 22);
+            this.tbName.TabIndex = 5;
             // 
-            // textBox2
+            // tbEmail
             // 
-            this.textBox2.Location = new System.Drawing.Point(337, 88);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(220, 20);
-            this.textBox2.TabIndex = 6;
+            this.tbEmail.Location = new System.Drawing.Point(449, 108);
+            this.tbEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(292, 22);
+            this.tbEmail.TabIndex = 6;
             // 
-            // textBox3
+            // tbPassword
             // 
-            this.textBox3.Location = new System.Drawing.Point(337, 143);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(220, 20);
-            this.textBox3.TabIndex = 7;
+            this.tbPassword.Location = new System.Drawing.Point(449, 176);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(292, 22);
+            this.tbPassword.TabIndex = 7;
             // 
-            // textBox4
+            // tbConfirmPassword
             // 
-            this.textBox4.Location = new System.Drawing.Point(337, 201);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(220, 20);
-            this.textBox4.TabIndex = 8;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.tbConfirmPassword.Location = new System.Drawing.Point(449, 247);
+            this.tbConfirmPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbConfirmPassword.Name = "tbConfirmPassword";
+            this.tbConfirmPassword.Size = new System.Drawing.Size(292, 22);
+            this.tbConfirmPassword.TabIndex = 8;
+            this.tbConfirmPassword.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
-            // button1
+            // btnSignup
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(215, 287);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 46);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Sign up";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignup.Location = new System.Drawing.Point(287, 353);
+            this.btnSignup.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(224, 57);
+            this.btnSignup.TabIndex = 9;
+            this.btnSignup.Text = "Sign up";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            this.btnSignup.Click += new System.EventHandler(this.btnSignup_Click);
             // 
-            // button2
+            // btnLogin
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(215, 418);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 37);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Log in";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(287, 514);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(224, 46);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Log in";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(203, 368);
+            this.label6.Location = new System.Drawing.Point(284, 453);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(191, 13);
+            this.label6.Size = new System.Drawing.Size(227, 16);
             this.label6.TabIndex = 11;
-            this.label6.Text = "Log oin if you already have an account";
+            this.label6.Text = "Log in if you already have an account";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.PeachPuff;
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnLogin);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnSignup);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.textBox4);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Location = new System.Drawing.Point(78, 93);
+            this.panel1.Controls.Add(this.tbConfirmPassword);
+            this.panel1.Controls.Add(this.tbName);
+            this.panel1.Controls.Add(this.tbPassword);
+            this.panel1.Controls.Add(this.tbEmail);
+            this.panel1.Location = new System.Drawing.Point(104, 114);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(601, 468);
+            this.panel1.Size = new System.Drawing.Size(801, 576);
             this.panel1.TabIndex = 12;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -180,18 +195,20 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.Black;
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(-7, -1);
+            this.panel2.Location = new System.Drawing.Point(-9, -1);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(854, 66);
+            this.panel2.Size = new System.Drawing.Size(1139, 81);
             this.panel2.TabIndex = 13;
             // 
             // Signup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 573);
+            this.ClientSize = new System.Drawing.Size(1125, 705);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Signup";
             this.Text = "Signup";
             this.panel1.ResumeLayout(false);
@@ -209,12 +226,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbConfirmPassword;
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
--- Travel_expense_Splitter/Signup.cs
+++ Travel_expense_Splitter/Signup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -45,6 +46,108 @@ namespace Travel_expense_Splitter
         private void label6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Form1 login= new Form1();
+            login.Show();
+            this.Close();
+        }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            string name = tbName.Text.Trim();
+            string email= tbEmail.Text.Trim();
+            string password= tbPassword.Text.Trim();
+            string confirmpassword = tbConfirmPassword.Text.Trim();
+
+
+            // Check for empty fields
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Name cannot be empty.");
+                tbName.Focus(); // Set focus back to the name textbox
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                MessageBox.Show("Email cannot be empty.");
+                tbEmail.Focus(); // Set focus back to the email textbox
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Password cannot be empty.");
+                tbPassword.Focus(); // Set focus back to the password textbox
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(confirmpassword))
+            {
+                MessageBox.Show("Please confirm your password.");
+                tbConfirmPassword.Focus(); // Set focus back to the confirm password textbox
+                return;
+            }
+
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("Please enter a valid email address.");
+                tbEmail.Focus();
+                return;
+            }
+
+            if (password != confirmpassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                tbPassword.Focus();
+                return;
+            }
+
+            string connectionString = "Server=CHINMAY-N3P5PKK\\SQLEXPRESS;Database=travel_expenses;Integrated Security=True;";
+
+            using (SqlConnection conn =new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into login_table (username,Email,pass) values (@name,@email,@password);";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@name", name);
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue ("@Password", password);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show(" Sign Up Successfully!");
+                            Form1 login = new Form1();
+                            login.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Sign Up Failed Due to some reason please try again later !!!");
+                        }
+                    }
+
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("Error :" +ex.Message);
+                }
+            }
+
+
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            // Simple regex for email validation
+            var emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return System.Text.RegularExpressions.Regex.IsMatch(email, emailPattern);
         }
     }
 }
