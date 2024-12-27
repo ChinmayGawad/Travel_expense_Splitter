40cd2aee0f6db9e3b506e8ed86bfc3079d86045f
Neel Pawaskar <pawaskarneel@outlook.com>
2024-12-27 10:15:25 +0530

    hello

--- Travel_expense_Splitter/Form2.Designer.cs
+++ Travel_expense_Splitter/Form2.Designer.cs
@@ -152,10 +152,11 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(684, 29);
+            this.label1.Location = new System.Drawing.Point(683, -23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(228, 51);
             this.label1.TabIndex = 14;
