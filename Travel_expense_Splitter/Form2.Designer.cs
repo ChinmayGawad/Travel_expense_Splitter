ceecee9732aa4b11fa5c4cc2112708fe194e7629
ApexKiller10k <vishwasave25@gmail.com>
2024-12-27 10:16:12 +0530

    hi

--- Travel_expense_Splitter/Form2.Designer.cs
+++ Travel_expense_Splitter/Form2.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(273, 531);
+            this.button1.Location = new System.Drawing.Point(205, 431);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(258, 64);
+            this.button1.Size = new System.Drawing.Size(194, 52);
             this.button1.TabIndex = 3;
             this.button1.Text = "Add Members";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,9 +59,10 @@
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(981, 529);
+            this.button2.Location = new System.Drawing.Point(736, 430);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(252, 64);
+            this.button2.Size = new System.Drawing.Size(189, 52);
             this.button2.TabIndex = 4;
             this.button2.Text = "Log Expesnes";
             this.button2.UseVisualStyleBackColor = true;
@@ -70,9 +72,10 @@
             // 
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(636, 529);
+            this.button3.Location = new System.Drawing.Point(477, 430);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(244, 66);
+            this.button3.Size = new System.Drawing.Size(183, 54);
             this.button3.TabIndex = 5;
             this.button3.Text = "View Balance";
             this.button3.UseVisualStyleBackColor = true;
@@ -84,9 +87,10 @@
             this.Total_Members.BackColor = System.Drawing.Color.Transparent;
             this.Total_Members.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Total_Members.ForeColor = System.Drawing.Color.Black;
-            this.Total_Members.Location = new System.Drawing.Point(101, 79);
+            this.Total_Members.Location = new System.Drawing.Point(76, 64);
+            this.Total_Members.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Total_Members.Name = "Total_Members";
-            this.Total_Members.Size = new System.Drawing.Size(78, 90);
+            this.Total_Members.Size = new System.Drawing.Size(64, 73);
             this.Total_Members.TabIndex = 6;
             this.Total_Members.Text = "  ";
             this.Total_Members.Click += new System.EventHandler(this.Total_Members_Click);
@@ -97,9 +101,10 @@
             this.Total_Expenses.BackColor = System.Drawing.Color.Transparent;
             this.Total_Expenses.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Total_Expenses.ForeColor = System.Drawing.Color.Black;
-            this.Total_Expenses.Location = new System.Drawing.Point(112, 69);
+            this.Total_Expenses.Location = new System.Drawing.Point(84, 56);
+            this.Total_Expenses.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Total_Expenses.Name = "Total_Expenses";
-            this.Total_Expenses.Size = new System.Drawing.Size(78, 90);
+            this.Total_Expenses.Size = new System.Drawing.Size(64, 73);
             this.Total_Expenses.TabIndex = 7;
             this.Total_Expenses.Text = "  ";
             // 
@@ -109,9 +114,10 @@
             this.panel3.BackColor = System.Drawing.SystemColors.Control;
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.Total_Members);
-            this.panel3.Location = new System.Drawing.Point(423, 198);
+            this.panel3.Location = new System.Drawing.Point(317, 161);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(291, 186);
+            this.panel3.Size = new System.Drawing.Size(218, 151);
             this.panel3.TabIndex = 12;
             // 
             // label2
@@ -120,9 +126,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(20, 20);
+            this.label2.Location = new System.Drawing.Point(15, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(227, 36);
+            this.label2.Size = new System.Drawing.Size(188, 29);
             this.label2.TabIndex = 13;
             this.label2.Text = "Total Members :";
             // 
@@ -133,9 +140,10 @@
             this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.Total_Expenses);
-            this.panel4.Location = new System.Drawing.Point(833, 198);
+            this.panel4.Location = new System.Drawing.Point(625, 161);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(301, 186);
+            this.panel4.Size = new System.Drawing.Size(226, 151);
             this.panel4.TabIndex = 13;
             // 
             // label4
@@ -144,37 +152,43 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(20, 20);
+            this.label4.Location = new System.Drawing.Point(15, 16);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(236, 36);
+            this.label4.Size = new System.Drawing.Size(192, 29);
             this.label4.TabIndex = 13;
             this.label4.Text = "Total Expenses :";
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(684, 29);
+            this.label1.Location = new System.Drawing.Point(513, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 51);
+            this.label1.Size = new System.Drawing.Size(183, 40);
             this.label1.TabIndex = 14;
             this.label1.Text = "Dashboard";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Dashbord
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MistyRose;
             this.BackgroundImage = global::Travel_expense_Splitter.Properties.Resources.Green_Full_Photo_Forest_Desktop_Wallpaper__1_1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1560, 813);
+            this.ClientSize = new System.Drawing.Size(1170, 661);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Dashbord";
             this.Text = "Form2";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
