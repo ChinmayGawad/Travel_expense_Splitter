bbeab73627135bd5c0640bc3311ff5e4a6c32f38
Neel Pawaskar <pawaskarneel@outlook.com>
2024-12-30 09:42:27 +0530

    neellll

--- Travel_expense_Splitter/Form5.Designer.cs
+++ Travel_expense_Splitter/Form5.Designer.cs
@@ -59,20 +59,19 @@
             this.panel2.Controls.Add(this.tbName);
             this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(639, 72);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Location = new System.Drawing.Point(895, 92);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(642, 658);
+            this.panel2.Size = new System.Drawing.Size(856, 810);
             this.panel2.TabIndex = 1;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(178, 497);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(237, 612);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(206, 21);
+            this.label6.Size = new System.Drawing.Size(268, 27);
             this.label6.TabIndex = 10;
             this.label6.Text = "Already have an account ?";
             // 
@@ -83,92 +82,88 @@
             this.btnSignup.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.btnSignup.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSignup.ForeColor = System.Drawing.Color.White;
-            this.btnSignup.Location = new System.Drawing.Point(91, 442);
-            this.btnSignup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSignup.Location = new System.Drawing.Point(121, 544);
+            this.btnSignup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSignup.Name = "btnSignup";
-            this.btnSignup.Size = new System.Drawing.Size(332, 36);
+            this.btnSignup.Size = new System.Drawing.Size(443, 44);
             this.btnSignup.TabIndex = 9;
-            this.btnSignup.Text = "SIgn Up";
+            this.btnSignup.Text = "Sign Up";
             this.btnSignup.UseVisualStyleBackColor = false;
             this.btnSignup.Click += new System.EventHandler(this.btnSignup_Click);
             // 
             // tbConfirmPassword
             // 
             this.tbConfirmPassword.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbConfirmPassword.Location = new System.Drawing.Point(80, 379);
-            this.tbConfirmPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbConfirmPassword.Location = new System.Drawing.Point(107, 466);
+            this.tbConfirmPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbConfirmPassword.Name = "tbConfirmPassword";
-            this.tbConfirmPassword.Size = new System.Drawing.Size(364, 32);
+            this.tbConfirmPassword.Size = new System.Drawing.Size(484, 38);
             this.tbConfirmPassword.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(76, 346);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(101, 426);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(160, 21);
+            this.label5.Size = new System.Drawing.Size(202, 27);
             this.label5.TabIndex = 7;
             this.label5.Text = "Confirm Password :";
             // 
             // tbPassword
             // 
             this.tbPassword.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPassword.Location = new System.Drawing.Point(80, 295);
-            this.tbPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbPassword.Location = new System.Drawing.Point(107, 363);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(363, 32);
+            this.tbPassword.Size = new System.Drawing.Size(483, 38);
             this.tbPassword.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(76, 262);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(101, 322);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 21);
+            this.label4.Size = new System.Drawing.Size(116, 27);
             this.label4.TabIndex = 5;
             this.label4.Text = "Password :";
             // 
             // tbEmail
             // 
             this.tbEmail.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbEmail.Location = new System.Drawing.Point(79, 209);
-            this.tbEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbEmail.Location = new System.Drawing.Point(105, 257);
+            this.tbEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbEmail.Name = "tbEmail";
-            this.tbEmail.Size = new System.Drawing.Size(364, 32);
+            this.tbEmail.Size = new System.Drawing.Size(484, 38);
             this.tbEmail.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(76, 176);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(101, 217);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 21);
+            this.label3.Size = new System.Drawing.Size(80, 27);
             this.label3.TabIndex = 3;
             this.label3.Text = "Email :";
             // 
             // tbName
             // 
             this.tbName.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbName.Location = new System.Drawing.Point(80, 130);
-            this.tbName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbName.Location = new System.Drawing.Point(107, 160);
+            this.tbName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(363, 32);
+            this.tbName.Size = new System.Drawing.Size(483, 38);
             this.tbName.TabIndex = 2;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(76, 96);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(101, 118);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(146, 21);
+            this.label7.Size = new System.Drawing.Size(192, 27);
             this.label7.TabIndex = 0;
             this.label7.Text = "Enter Your Name :";
             // 
@@ -181,9 +176,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Blue;
-            this.button1.Location = new System.Drawing.Point(215, 522);
+            this.button1.Location = new System.Drawing.Point(287, 642);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.Size = new System.Drawing.Size(100, 46);
             this.button1.TabIndex = 14;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -191,14 +187,14 @@
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MistyRose;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1058, 716);
+            this.ClientSize = new System.Drawing.Size(1496, 886);
             this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form5";
             this.Text = "Form5";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
--- Travel_expense_Splitter/addTrip.Designer.cs
+++ Travel_expense_Splitter/addTrip.Designer.cs
@@ -21,47 +21,78 @@
             this.tbTripName = new System.Windows.Forms.TextBox();
             this.btnAddTrip = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbTripName
             // 
-            this.tbTripName.Location = new System.Drawing.Point(100, 50);
+            this.tbTripName.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTripName.Location = new System.Drawing.Point(37, 162);
+            this.tbTripName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbTripName.Name = "tbTripName";
-            this.tbTripName.Size = new System.Drawing.Size(200, 20);
+            this.tbTripName.Size = new System.Drawing.Size(454, 45);
             this.tbTripName.TabIndex = 0;
             // 
             // btnAddTrip
             // 
-            this.btnAddTrip.Location = new System.Drawing.Point(100, 100);
+            this.btnAddTrip.BackColor = System.Drawing.Color.White;
+            this.btnAddTrip.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnAddTrip.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTrip.Location = new System.Drawing.Point(169, 267);
+            this.btnAddTrip.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddTrip.Name = "btnAddTrip";
-            this.btnAddTrip.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTrip.Size = new System.Drawing.Size(183, 49);
             this.btnAddTrip.TabIndex = 1;
             this.btnAddTrip.Text = "Add Trip";
-            this.btnAddTrip.UseVisualStyleBackColor = true;
+            this.btnAddTrip.UseVisualStyleBackColor = false;
             this.btnAddTrip.Click += new System.EventHandler(this.btnAddTrip_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(100, 30);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(174, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.Size = new System.Drawing.Size(187, 42);
             this.label1.TabIndex = 2;
             this.label1.Text = "Trip Name";
             // 
-            // FormAddTrip
+            // panel1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.btnAddTrip);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbTripName);
+            this.panel1.Location = new System.Drawing.Point(492, 237);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(517, 433);
+            this.panel1.TabIndex = 3;
+            // 
+            // addTrip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 200);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnAddTrip);
-            this.Controls.Add(this.tbTripName);
-            this.Name = "FormAddTrip";
+            this.BackgroundImage = global::Travel_expense_Splitter.Properties.Resources.Green_Full_Photo_Forest_Desktop_Wallpaper__1_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1490, 836);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "addTrip";
             this.Text = "Add Trip";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.addTrip_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
+
         }
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
